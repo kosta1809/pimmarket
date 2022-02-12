644750bcf23e2395caa2d8eda23f25c10ef1ca47
Zardar <doomiurg@gmail.com>
2022-02-12 13:18:07 +0500

    add save itemlist to file add load itemlist from file

--- pimscaninventory.lua
+++ pimscaninventory.lua
@@ -2,7 +2,7 @@
 --pim event player_on name address address
 --pim event player_off name address address
 --pim getStackInSlot:table witch fields k+v: display_name,dmg,id,max_dmg,max_size,mod_id,name,ore_dict,qty,raw_name//whre qty is amount
-pim=require('component').pim
+--fields form item: display_name, id, raw_name. also need add price for bye, price for cell. may be in 2 custom listuire('component').pim
 local item,items='',{}
 
 
@@ -16,15 +16,46 @@ for f=1,40 do if items[f] then
 	print(items[f].display_name,items[f].id,items[f].qty)
 end end
 
---read db from file--
-local items={}
-local index=1
-db=io.open('db','r')
-if db then
-	for f in db do items[f]={} 
-		items[f].id=db:read('*line')
-		items[f].price=db:read('*line')
-		items[f].display_name=db:read('*line')
-		index=index+1
+
+--I have an idea for create table by id of item
+--load db from file by id
+loadFromFile=function(itemlist)
+    if 'table'~=type(itemlist) then itemlist={} end
+	local db=io.open('db','r')
+	if db then
+		local size=db:read('*line')
+		itemlist.size=size
+		for f=1, size do 
+			id=db:read('*line')
+			itemlist[id]={}
+			itemlist[id].display_name=db:read('*line')
+			itemlist[id].sell_price=db:read('*line')
+			itemlist[id].bye_price=db:read('*line')
+			itemlist[id].raw_name=db:read('*line')
+		end
 	end
+	return itemlist
 end
+
+saveTableToFile=function(itemlist)
+--creating items list and build by id of item
+
+db=io.open('db','w')
+db:write(itemlist.size..'\n')
+local size=itemlist.size
+itemlist.size=nil
+for f
+
+
+
+
+
+
+
+
+
+
+
+
+
+

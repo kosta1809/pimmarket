c11e36224bc303437d6d08c9b6418734e2e995dd
Zardar <doomiurg@gmail.com>
2022-02-20 16:28:14 +0500

    optimize

--- loader.lua
+++ loader.lua
@@ -1,18 +1,5 @@
 local loader_v='0.5.0'--for check version
-local market_v='0.5.0'
 local inet=require('internet')
-local component=require('component')
-local event=require('event')
-
---fs it is filesystem
-for address in component.list("filesy") do
-    if component.proxy(address).spaceTotal() > 66000  and component.proxy(address).exists('init.lua') then
-        local fs = component.proxy(address)
-    end
-end
-if not fs then
-  print("Filesystem not found!")
-end
 
 --download files
 function wget(url,name)
@@ -25,7 +12,6 @@ function wget(url,name)
 end
 
 --in this place need add block: check updates available
---if veersios < this.versions then download new else nothing end
 wget('https://raw.githubusercontent.com/Zardar/pimmarket/master/loader.lua','loader.lua')
 wget('https://raw.githubusercontent.com/Zardar/pimmarket/master/pimmarket.lua','market.lua')
 

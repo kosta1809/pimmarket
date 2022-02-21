e63790cd5fb25179392bed3257691c8726e2d733
Zardar <doomiurg@gmail.com>
2022-02-21 20:28:49 +0500

    ......

--- pimmarket.lua
+++ pimmarket.lua
@@ -385,6 +385,7 @@ function market.merge()
 			market.itemlist[id].display_name=market.chestList[id].display_name
 			market.itemlist.size=market.itemlist.size+1
 		else
+			print (market.itemlist[id].qty)
 			local qty = market.itemlist[id].qty
 			qty = qty + market.chestList[id].qty
 			market.itemlist[id].qty= qty 
